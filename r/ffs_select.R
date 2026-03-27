84eba26514883b378a056d63a97569444fc3cd26
Camille Piponiot <camille.piponiot@gmail.com>
2026-03-27 21:01:40 +0000

    updated RF method and map - now includes: soc content as predicted variable / landuse and deforestation age as predictors

--- r/ffs_select.R
+++ r/ffs_select.R
@@ -1,6 +1,6 @@
 ffs_select <- function(vars, data, select = NULL, response = "T_stock") {
   if (is.null(select)) select <- rep(TRUE, length(vars))
-  if (length(vars) == 1) {
+  if (length(vars) <= 2) {
     return(TRUE)
   } else {
     ffsel <- data[, vars[select]] |>
--- r/kfold.R
+++ r/kfold.R
@@ -10,7 +10,7 @@ kfold <- function(data, var = "T_stock", outfold, fun_model, fun_pred) {
     )
     obs_outfold <- subset(data, get(outfold) == i)[, var]
     obs_infold_mean <- mean(subset(data, get(outfold) != i)[, var])
-    return(cbind(obs_outfold, pred_outfold, obs_infold_mean)) #nolint
+    return(cbind(obs_outfold, pred_outfold, obs_infold_mean)) # nolint
   })
   rmse <-
     lapply(obs_pred, function(x) sqrt(sum((x[, 1] - x[, 2])^2) / nrow(x))) |>
